08488b0f6be128cced065492ea890d85f698a65b
whirmill <lorenzo@bemind.me>
2024-11-06 18:03:12 +0100

    Bump versions: - App version: 0.6.0-beta.12 - Backend version: 0.6.0-beta.12 - Discovery version: 0.6.0-beta.12 - Mock version: 0.6.0-beta.12 - Frontend version: 0.6.0-beta.12

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -11,7 +11,6 @@ export function middleware(req: NextRequest) {
     // Crea la risposta riscrivendo l'URL
     const response = NextResponse.rewrite(grafanaUrl);
 
-    console.log(url);
     if (!url.pathname.startsWith("/grafana/public-dashboards")) {
       // Aggiungi gli headers personalizzati per le richieste verso Grafana
       response.headers.set("X-WEBAUTH-USER", "admin");
