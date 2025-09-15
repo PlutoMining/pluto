fb20f853808d73d5dfa860e62f2df3b9e661dcff
Vittorio Cecchetto <v.cecchetto@gmail.com>
2025-09-15 18:03:37 +0200

    Bump versions:     - App version: 0.7.0-rc.12     - Backend version: 0.7.0-rc.12     - Discovery version: 0.7.0-rc.12     - Frontend version: 0.7.0-rc.12     - Grafana version: 0.7.0-rc.12     - Prometheus version: 0.7.0-rc.12

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DOCKER_REGISTRY=registry.gitlab.com/plutomining/pluto
-
 # Function to update the version in the specified files
 update_version() {
     local service=$1
@@ -9,11 +7,11 @@ update_version() {
     local image_sha=$3
 
     # Update docker-compose files with the correct image and SHA
-    sed -i -E "s/plutomining\/pluto\/pluto-$service(\:[^\s]+)/plutomining\/pluto\/pluto-$service:${new_version}/g" umbrel-apps/pluto-next/docker-compose.yml
-    sed -i -E "s/plutomining\/pluto\/pluto-$service(\:[^\s]+)/plutomining\/pluto\/pluto-$service:${new_version}/g" docker-compose.next.local.yml
+    sed -i -E "s|(plutomining/pluto/pluto-$service):[^[:space:]]+|\1:${new_version}@${image_sha}|g" umbrel-apps/pluto-next/docker-compose.yml
+    sed -i -E "s|(plutomining/pluto/pluto-$service):[^[:space:]]+|\1:${new_version}@${image_sha}|g" docker-compose.next.local.yml
 
-    sed -i -E "s/plutomining\/pluto\/pluto-$service(\:[^\s]+)/plutomining\/pluto\/pluto-$service:${new_version}/g" umbrel-apps/pluto/docker-compose.yml
-    sed -i -E "s/plutomining\/pluto\/pluto-$service(\:[^\s]+)/plutomining\/pluto\/pluto-$service:${new_version}/g" docker-compose.release.local.yml
+    sed -i -E "s|(plutomining/pluto/pluto-$service):[^[:space:]]+|\1:${new_version}@${image_sha}|g" umbrel-apps/pluto/docker-compose.yml
+    sed -i -E "s|(plutomining/pluto/pluto-$service):[^[:space:]]+|\1:${new_version}@${image_sha}|g" docker-compose.release.local.yml
 
 #    sed -i -E "s/plutomining\/pluto\/pluto-$service(\:[^\s]+(@sha256:[a-f0-9]+))/plutomining\/pluto\/pluto-$service:${new_version}@${image_sha}/g" umbrel-apps/pluto/docker-compose.yml
 }
@@ -64,108 +62,117 @@ get_image_sha() {
     echo "$sha"
 }
 
-# Default values for flags
-SKIP_LOGIN=false
+# main
 
-# Parse command line arguments
-for arg in "$@"; do
-    case $arg in
-    --skip-login)
-        SKIP_LOGIN=true
-        shift # Remove --skip-login from processing
-        ;;
-    esac
-done
+main() {
+    DOCKER_REGISTRY=registry.gitlab.com/plutomining/pluto
 
-# Only perform Docker login if the skip login flag is not set
-if [ "$SKIP_LOGIN" = false ]; then
-    echo "Enter your Docker personal access token:"
-    read -s DOCKER_ACCESS_TOKEN
+    # Default values for flags
+    SKIP_LOGIN=false
 
-    # Perform Docker login
-    echo "$DOCKER_ACCESS_TOKEN" | docker login $DOCKER_REGISTRY
-else
-    echo "Skipping Docker login..."
-fi
+    # Parse command line arguments
+    for arg in "$@"; do
+        case $arg in
+        --skip-login)
+            SKIP_LOGIN=true
+            shift # Remove --skip-login from processing
+            ;;
+        esac
+    done
 
-# Prompt for the new version of the app (umbrel-app.yml stable or next)
-current_app_version=$(get_current_app_version)
-echo "Current app version is $current_app_version. Enter the new app version (press Enter to keep $current_app_version):"
-read new_app_version
+    # Only perform Docker login if the skip login flag is not set
+    if [ "$SKIP_LOGIN" = false ]; then
+        echo "Enter your Docker personal access token:"
+        read -s DOCKER_ACCESS_TOKEN
 
-# If user presses enter, keep the current version
-if [ -z "$new_app_version" ]; then
-    new_app_version=$current_app_version
-fi
-
-# Update the version in umbrel-app.yml
-update_umbrel_version "$new_app_version"
-
-# Get and set versions for each service
-for service in backend discovery frontend grafana prometheus; do
-    current_version=$(get_current_version $service)
-    eval "current_${service}_version=$current_version"
-
-    echo "Current version for $service is $current_version. Enter the new version (press Enter to keep $current_version):"
-    read new_version
-
-    if [ -z "$new_version" ]; then
-        new_version=$current_version
-    fi
-
-    eval "${service}_version=$new_version"
-done
-
-# Update the files with the new versions and install dependencies
-for service in backend discovery frontend grafana prometheus; do
-    eval new_version=\$${service}_version
-    eval current_version=\$current_${service}_version
-
-    if [ "$new_version" != "$current_version" ]; then
-
-        # Update the version in the service's package.json
-        sed -i -E "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z]+(\.[0-9]+)?)?\"/\"version\": \"${new_version}\"/" $service/package.json
-
-        # Run npm install in the service to update lockfile
-        echo "Running npm install in $service..."
-        (cd $service && npm install)
-
-        echo "Building Docker image for $service with context $(pwd) and Dockerfile $service/Dockerfile..."
-
-        # Build the Docker image with the specified version tags
-        docker buildx build --platform linux/amd64,linux/arm64 \
-            -t ${DOCKER_REGISTRY}/pluto-$service:latest \
-            -t ${DOCKER_REGISTRY}/pluto-$service:"$new_version" \
-            -f $service/Dockerfile . --push
-
-        # Get the updated SHA after build
-        image_sha=$(get_image_sha "${DOCKER_REGISTRY}/pluto-$service:${new_version}")
+        # Perform Docker login
+        echo "$DOCKER_ACCESS_TOKEN" | docker login $DOCKER_REGISTRY
     else
-        echo "Skipping Docker build for $service as the version has not changed."
-        # Get the existing SHA of the current image
-        image_sha=$(get_image_sha "${DOCKER_REGISTRY}/pluto-$service:${new_version}")
+        echo "Skipping Docker login..."
     fi
 
-    # Update files with the SHA for the service
-    update_version $service "$new_version" "$image_sha"
-done
+    # Prompt for the new version of the app (umbrel-app.yml stable or next)
+    current_app_version=$(get_current_app_version)
+    echo "Current app version is $current_app_version. Enter the new app version (press Enter to keep $current_app_version):"
+    read new_app_version
 
-# Stage all changes
-echo "Staging changes..."
-git add -A .
+    # If user presses enter, keep the current version
+    if [ -z "$new_app_version" ]; then
+        new_app_version=$current_app_version
+    fi
 
-# Commit the changes with a message
-echo "Committing changes..."
-git commit -m "Bump versions:
-- App version: ${new_app_version}
-- Backend version: ${backend_version}
-- Discovery version: ${discovery_version}
-- Frontend version: ${frontend_version}
-- Grafana version: ${grafana_version}
-- Prometheus version: ${prometheus_version}"
+    # Update the version in umbrel-app.yml
+    update_umbrel_version "$new_app_version"
 
-# Push the Git changes
-echo "Pushing changes to the repository..."
-git push
+    # Get and set versions for each service
+    for service in backend discovery frontend grafana prometheus; do
+        current_version=$(get_current_version $service)
+        eval "current_${service}_version=$current_version"
 
-echo "Process completed successfully!"
+        echo "Current version for $service is $current_version. Enter the new version (press Enter to keep $current_version):"
+        read new_version
+
+        if [ -z "$new_version" ]; then
+            new_version=$current_version
+        fi
+
+        eval "${service}_version=$new_version"
+    done
+
+    # Update the files with the new versions and install dependencies
+    for service in backend discovery frontend grafana prometheus; do
+        eval new_version=\$${service}_version
+        eval current_version=\$current_${service}_version
+
+        if [ "$new_version" != "$current_version" ]; then
+
+            # Update the version in the service's package.json
+            sed -i -E "s/\"version\": \"[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z]+(\.[0-9]+)?)?\"/\"version\": \"${new_version}\"/" $service/package.json
+
+            # Run npm install in the service to update lockfile
+            echo "Running npm install in $service..."
+            (cd $service && npm install)
+
+            echo "Building Docker image for $service with context $(pwd) and Dockerfile $service/Dockerfile..."
+
+            # Build the Docker image with the specified version tags
+            docker buildx build --platform linux/amd64,linux/arm64 \
+                -t ${DOCKER_REGISTRY}/pluto-$service:latest \
+                -t ${DOCKER_REGISTRY}/pluto-$service:"$new_version" \
+                -f $service/Dockerfile . --push
+
+            # Get the updated SHA after build
+            image_sha=$(get_image_sha "${DOCKER_REGISTRY}/pluto-$service:${current_version}")
+        else
+            echo "Skipping Docker build for $service as the version has not changed."
+            # Get the existing SHA of the current image
+            image_sha=$(get_image_sha "${DOCKER_REGISTRY}/pluto-$service:${current_version}")
+        fi
+
+        # Update files with the SHA for the service
+        update_version $service "$new_version" "$image_sha"
+    done
+
+    # Stage all changes
+    echo "Staging changes..."
+    git add -A .
+
+    # Commit the changes with a message
+    echo "Committing changes..."
+    git commit -m "Bump versions:
+    - App version: ${new_app_version}
+    - Backend version: ${backend_version}
+    - Discovery version: ${discovery_version}
+    - Frontend version: ${frontend_version}
+    - Grafana version: ${grafana_version}
+    - Prometheus version: ${prometheus_version}"
+
+    # Push the Git changes
+    echo "Pushing changes to the repository..."
+    git push
+
+    echo "Process completed successfully!"
+
+}
+
+main
