dcf22e16d9888682260aca754adfa5283295f087
Pietro Pollarolo <pietro.pollarolo@gmail.com>
2025-11-26 10:06:22 +0100

    Configure CI tools for discovery app

---
+++ discovery/.eslintrc.cjs
@@ -0,0 +1,16 @@
+const path = require('path');
+const baseConfig = require('../tooling/eslint.base.cjs');
+
+module.exports = {
+  ...baseConfig,
+  root: true,
+  parserOptions: {
+    ...baseConfig.parserOptions,
+    project: path.resolve(__dirname, 'tsconfig.json'),
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    ...baseConfig.env,
+  },
+};
+
